e7ba25bfc2a27859ef774fdae8b395fd5e00b078
indritkalaj <kalaj.indrit@gmail.com>
2021-10-04 16:52:59 +0200

    Fixes

--- main.py
+++ main.py
@@ -35,25 +35,27 @@ import pyodbc
 st.set_page_config(layout="wide")
 
 st.title('Data Import from DB using Streamlit')
-st.text('The following data are for internal use only! Please do not distribute the data outside the group of interest!')
+st.info('The following data are for internal use only! Please do not distribute the data outside the group of interest!')
 
-
-# sql_conn = pyodbc.connect('DRIVER={ODBC Driver 17 for SQL Server};SERVER=ALTIRDT0407WZ2;DATABASE=al_datathon_amz;Trusted_Connection=yes')
-# query = "SELECT * FROM [al_datathon_amz].[dbo].[raw_data];"
-# df = pd.read_sql(query, sql_conn)
 
 #AgGrid(df, height=500)
 st.write('\n')
 st.write('\n')
 st.subheader('Show / Hide RAW Data 💹' )
 
+
+
 months = ("January","February","March","April","May","June","July","August","September","October","November","December")
+years = ("2020","2021","2022")
 mtd = st.selectbox('Select month as filter',months)
+ytd = st.selectbox('Select year as filter', years)
 month_selection = months.index(mtd) + 1
 
 sql_conn = pyodbc.connect('DRIVER={ODBC Driver 17 for SQL Server};SERVER=ALTIRDT0407WZ2;DATABASE=al_datathon_amz;Trusted_Connection=yes')
-query = "SELECT * FROM [al_datathon_amz].[dbo].[raw_data] where [activity_month] = 8"
-query = query.replace("8", str(month_selection))
+query = '''
+SELECT * FROM [al_datathon_amz].[dbo].[raw_data] where [activity_month] = 8 and [activity_date] like '%2020%'
+'''
+query = query.replace( "8", str(month_selection)).replace("2020", str(ytd) )
 df = pd.read_sql(query, sql_conn)
 
 
@@ -63,7 +65,7 @@ if is_check:
     #st.text('The data can be sorted by column and columns can be filtered by value.')
     #AgGrid(df, height=500, sideBar=True, dndSource=True, suppressCsvExport=False)
     st.write(df)
-file_name = "df.csv"
+file_name = "raw data.csv"
 file_path = f"./{file_name}"
 df.to_csv(file_path)
 # Create Download Button
@@ -119,22 +121,18 @@ st.text('The download button will download stats over the data table.')
 st.write('\n')
 st.write('\n')
 st.subheader('Pivoting the RAW Data below:' )
+st.info('Use the filters below for the pivot table only!')
+
+rap_df_pivot = df.copy()
+
+rap_df_pivot['RAP'] = (rap_df_pivot['hmd_response_yes_adjusted'].astype('int64') / (rap_df_pivot['hmd_response_yes_adjusted'].astype('int64') + rap_df_pivot['hmd_response_no_adjusted'].astype('int64')))
+rap_df_pivot['RAP'] = pd.Series(["{0:.2f}%".format(val * 100) for val in rap_df_pivot['RAP']])
+rap_df_pivot['CCX_U'] = (rap_df_pivot['understandability_45count'].astype('int64') / rap_df_pivot['understandability_12345count'].astype('int64'))
+rap_df_pivot['CCX_U'] = pd.Series(["{0:.2f}%".format(val * 100) for val in rap_df_pivot['CCX_U']])
 
 
-sql_conn = pyodbc.connect('DRIVER={ODBC Driver 17 for SQL Server};SERVER=ALTIRDT0407WZ2;DATABASE=al_datathon_amz;Trusted_Connection=yes')
-query = "SELECT * FROM [al_datathon_amz].[dbo].[raw_data] where [activity_month] = 8"
-query = query.replace("8", str(month_selection))
-rap_df = pd.read_sql(query, sql_conn)
-
-
-rap_df['RAP'] = (rap_df['hmd_response_yes_adjusted'].astype('int64') / (rap_df['hmd_response_yes_adjusted'].astype('int64') + rap_df['hmd_response_no_adjusted'].astype('int64')))
-rap_df['CCX_U'] = (rap_df['understandability_45count'].astype('int64') / rap_df['understandability_12345count'].astype('int64'))
-rap_df['Channel'] = rap_df['contact_type']
-
-
-rap_df = rap_df.style.format({'RAP': "{:.2f}",'CCX_U': "{:.2f}"})
-st.dataframe(rap_df)
-rap_df_pivot = pd.pivot_table(rap_df, index=['manager_login_lvl_1', 'login_name'] )
+#rap_df_pivot = rap_df_pivot.style.format({'RAP': "{:.2f}",'CCX_U': "{:.2f}"})
+#rap_df_pivot = pd.pivot_table(rap_df_pivot, index=['manager_login_lvl_1', 'login_name'] )
 
 
 ############################################################################################
@@ -143,23 +141,30 @@ rap_df_pivot = pd.pivot_table(rap_df, index=['manager_login_lvl_1', 'login_name'
 
 
 st.cache()
-contact_type = list(rap_df['Channel'].drop_duplicates())
-agent_name = list(rap_df['login_name'].drop_duplicates())
-team_manager = list(rap_df['manager_login_lvl_1'].drop_duplicates())
-activity_week = list(df['activity_week'].drop_duplicates())
-activity_month = list(df['activity_month'].drop_duplicates())
-activity_date = list(df['activity_date'].drop_duplicates())
-# #data_col = data['Data'][(data['Geography'] == Country_choice) & (data['Category'] == Category_choice) & (data['Series'] == Series_choice) & (data['Data Type'] == Data_type_choice)].unique()
-st.sidebar.subheader('Use this filters for the pivot table only!')
-#contact_type_choice = []
-#contact_type_choice = st.sidebar.multiselect('Contact Type: ', contact_type, default=contact_type)
-# agent_name_choice = st.sidebar.selectbox('Agent Name:', agent_name)
-# team_manager_choice = st.sidebar.selectbox('Team Leader Name:', team_manager)
-# activity_week_choice = st.sidebar.selectbox('Week:', activity_week)
-# activity_month_choice = st.sidebar.selectbox('Month:', activity_month)
-# activity_date = st.sidebar.selectbox('Date:', activity_date)
+contact_type = list(rap_df_pivot['contact_type'].drop_duplicates())
+#agent_name = list(rap_df_pivot['login_name'].drop_duplicates())
+team_manager = list(rap_df_pivot['manager_login_lvl_1'].drop_duplicates())
+activity_month = list(rap_df_pivot['activity_month'].drop_duplicates())
+activity_week = list(rap_df_pivot['activity_week'].drop_duplicates())
+activity_date = list(rap_df_pivot['activity_date'].drop_duplicates())
 
-#rap_df_pivot = rap_df_pivot.loc[rap_df_pivot['Channel'].isin(contact_type_choice)]
+#st.sidebar.subheader('Use this filters for the pivot table only!')
+
+contact_type_choice = st.multiselect('Contact Type: ', contact_type, default=contact_type)
+team_manager_choice = st.multiselect('Team Leader Name: ', team_manager)
+#agent_name_choice = st.multiselect('Agent Name: ', agent_name, default=[])
+activity_month_choice = st.multiselect('Month:', activity_month)
+activity_week_choice = st.multiselect('Week:', activity_week)
+activity_date_choice = st.multiselect('Date:', activity_date)
+
+#rap_df_pivot = rap_df_pivot.loc[rap_df_pivot['contact_type'].isin(contact_type_choice)]
+rap_df_pivot = rap_df_pivot[
+    (rap_df_pivot.contact_type.isin(contact_type_choice)) &
+    (rap_df_pivot.manager_login_lvl_1.isin(team_manager_choice)) &
+    (rap_df_pivot.activity_month.isin(activity_month_choice)) &
+    (rap_df_pivot.activity_week.isin(activity_week_choice)) &
+    (rap_df_pivot.activity_date.isin(activity_date_choice))
+]
 #rap_pivot_df = rap_pivot_df[rap_pivot_df['contact_type'] == 'phone']
 
 st.dataframe(rap_df_pivot)
